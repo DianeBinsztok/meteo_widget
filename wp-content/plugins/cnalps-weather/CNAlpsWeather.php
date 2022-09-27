30b8a7bc4df7d713669d6fb3508472e0d18fd0be
DianeBinsztok <diane.binsztok@le-campus-numerique.fr>
2022-09-27 14:03:53 +0200

    feat: update and render widget's form data on the page

--- wp-content/plugins/cnalps-weather/CNAlpsWeather.php
+++ wp-content/plugins/cnalps-weather/CNAlpsWeather.php
@@ -22,75 +22,93 @@ function __construct() {
 	);
 }
  
-// Display the widget
+   // Affichage du widget sur le rendu de la page
 public function widget( $args, $instance ) {
 
 	extract( $args );
+	
 
 	// Check the widget options
-	$title    = isset( $instance['title'] ) ? apply_filters( 'widget_title', $instance['title'] ) : '';
-	$text     = isset( $instance['text'] ) ? $instance['text'] : '';
-	$textarea = isset( $instance['textarea'] ) ?$instance['textarea'] : '';
-	$select   = isset( $instance['select'] ) ? $instance['select'] : '';
-	$checkbox = ! empty( $instance['checkbox'] ) ? $instance['checkbox'] : false;
+	$title = isset( $instance['title'] ) ? apply_filters( 'widget_title', $instance['title'] ) : '';
+	$contry = isset( $instance['country'] ) ? $instance['country'] : '';
+	$city = isset( $instance['city'] ) ?$instance['city'] : '';
 
 	// WordPress core before_widget hook (always include )
 	echo $before_widget;
 
-   // Display the widget
-   echo '<div class="widget-text wp_widget_plugin_box">';
 
+   echo '<aside class="widget-text wp_widget_plugin_box">';
 		// Display widget title if defined
 		if ( $title ) {
-			echo $before_title . $title . $after_title;
+			echo '<h4>'. $before_title . $title . $after_title.'</h4></br>';
+		}else{
+			echo '<h4>Météo</h4></br>';
 		}
 
 		// Display text field
-		if ( $text ) {
-			echo '<p>' . $text . '</p>';
+		if ( $contry ) {
+			echo '<p>' . $contry . '</p>';
 		}
 
 		// Display textarea field
-		if ( $textarea ) {
-			echo '<p>' . $textarea . '</p>';
+		if ( $city ) {
+			echo '<p>' . $city . '</p>';
 		}
 
-		// Display select field
-		if ( $select ) {
-			echo '<p>' . $select . '</p>';
-		}
-
-		// Display something if checkbox is true
-		if ( $checkbox ) {
-			echo '<p>Something awesome</p>';
-		}
-
-	echo '</div>';
+	echo '</aside>';
 
 	// WordPress core after_widget hook (always include )
 	echo $after_widget;
 
 }
  
-// back
+// affichage du widget sur le dashboard
 public function form( $instance ) {
- // Set widget defaults
- print_r($instance);
+
+		// Set widget defaults
+		$defaults = array(
+			'title'    => 'Météo',
+			'country'     => '',
+			'city' => '',
+		);
+		
+		// Parse current settings with defaults
+		extract( wp_parse_args( ( array ) $instance, $defaults ) ); 
+
+ //$country = $instance['country'] ?? '';
+ //$city = $instance['city'] ?? '';
+ ?>
+
+ <div>
+ <label for="<?= $this->get_field_id('country') ?>">Pays</label>
+ <input type="text" name="<?= $this->get_field_name('country') ?>" id="<?= $this->get_field_id('country') ?>"
+value="<?= esc_attr($country) ?>">
+ </div>
+ <div>
+ <label for="<?= $this->get_field_id('city') ?>">Ville</label>
+ <input type="text" name="<?= $this->get_field_name('city') ?>" id="<?= $this->get_field_id('city') ?>"
+  value="<?= esc_attr($city) ?>">
+ </div>
+
+ <?php
 }
  
+
+
 // update
 public function update( $new_instance, $old_instance ) {
-	$instance = $old_instance;
-	$instance['title']    = isset( $new_instance['title'] ) ? wp_strip_all_tags( $new_instance['title'] ) : '';
-	$instance['text']     = isset( $new_instance['text'] ) ? wp_strip_all_tags( $new_instance['text'] ) : '';
-	$instance['textarea'] = isset( $new_instance['textarea'] ) ? wp_kses_post( $new_instance['textarea'] ) : '';
-	$instance['checkbox'] = isset( $new_instance['checkbox'] ) ? 1 : false;
-	$instance['select']   = isset( $new_instance['select'] ) ? wp_strip_all_tags( $new_instance['select'] ) : '';
-	return $instance;
+    $instance = $old_instance;
+	$instance['country']     = isset( $new_instance['country'] ) ? wp_kses_post( $new_instance['country'] ) : '';
+	$instance['city'] = isset( $new_instance['city'] ) ? wp_kses_post( $new_instance['city'] ) : '';
+    echo("instance => ".$instance);
+    return $instance;
 }
 }
+
 function CNAlpsWeather_load_widget() {
     register_widget( 'CNAlpsWeather' );
 }
 add_action( 'widgets_init', 'CNAlpsWeather_load_widget' );
 ?>
+
+    
